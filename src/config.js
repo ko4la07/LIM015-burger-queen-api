9f93c4768f56425e315a4cfd6cc5f605f2236b24
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2021-10-04 08:46:19 -0500

    fix :D

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Variables de entorno (que por def son variables de conf global)
-exports.port = process.argv[2] || process.env.PORT || 8080;
+exports.port = process.env.PORT || 8080;
 exports.dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/test'; // cadena de conexion
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
